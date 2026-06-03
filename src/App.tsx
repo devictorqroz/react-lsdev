10f5378af81ea6af75f2c498d5184747f92d4662
Victor Queiroz <victorqueiroz.ti@gmail.com>
2026-06-02 22:17:54 -0300

    feat: #03 - introduction components and hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 export function App() {
 
@@ -5,6 +6,43 @@ export function App() {
     <>
       Olá
     </>
+  )
+}
+
+
+// JS Function
+const test = () => {
+  return 1 + 1;
+}
+
+
+// JS Function
+const useTest = () => {
+  return 1 + 1;
+}
+
+
+// React Hook 
+const useTeste = () => {
+  const [value] = useState(1 + 1);
+  
+  return value;
+}
+
+
+// React Functional Component
+const MyParagraph = () => {
+  return (
+    <p>Lorem ipsum</p>
+  )
+}
+
+
+// JS Function that return "html react"
+// Bad idea
+const myParagraph = () => {
+  return (
+    <p>Lorem ipsum</p>
   )
 }
 

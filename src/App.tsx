c807bc7058f5e191707e71d672dc0abbce271de8
Victor Queiroz <victorqueiroz.ti@gmail.com>
2026-06-27 01:51:15 -0300

    feat: create TodoAPI methods to mock server with mirage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import { useState } from "react"
 
+import { TodoAPI } from "./shared/services/api/TodoAPI";
 import { InputAdd } from "./components/InputAdd";
 import { TodoItem } from "./components/TodoItem";
 import { List } from "./components/List";
+
+
+TodoAPI.getAll().then(data => console.log('1', data));
+
+TodoAPI.create({ label: 'Devocional', complete: false });
+TodoAPI.create({ label: 'Oração', complete: false });
+
+TodoAPI.getAll().then(data => console.log('2', data));
+
+TodoAPI.updateById('1', { label: 'Leitura Noite', complete: false });
+
+TodoAPI.getAll().then(data => console.log('3', data));
+
+TodoAPI.deleteById('1');
+
+TodoAPI.getAll().then(data => console.log('4', data));
+
 
 
 interface ITodo {

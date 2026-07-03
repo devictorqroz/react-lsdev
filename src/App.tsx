780503e046d2d05677a23ec87b5c19ded07276d4
Victor Queiroz <victorqueiroz.ti@gmail.com>
2026-07-03 13:59:18 -0300

    feat: Apply routing with declarative react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
+
 import { AppLayout } from "./shared/layout/AppLayout";
 import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+
 
 
 
 export function App() {
 
   return (
-    <AppLayout>
-      <Home />
-    </AppLayout>
+    <BrowserRouter>
+      <AppLayout>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/sobre' element={<About />} />
+          
+          <Route path='*' element={<Navigate to='/' />} />
+        </Routes>
+      </AppLayout>
+    </BrowserRouter>
   );
 }

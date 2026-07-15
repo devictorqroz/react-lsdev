7e72af7696284804b19704a931aabba574ddd8fc
Victor Queiroz <victorqueiroz.ti@gmail.com>
2026-07-15 08:50:39 -0300

    feat: ra #04 - Add react hook form

--- src/pages/private/todos/TodoDetail.tsx
+++ src/pages/private/todos/TodoDetail.tsx
@@ -1,16 +1,31 @@
 import { useParams } from "react-router";
+import { useForm } from "react-hook-form";
 
 import { PageLayout } from "../../../shared/layout/page-layout/PageLayout";
-
 
 
 export const TodoDetail = () => {
     const { id } = useParams();
 
+    const { register, handleSubmit } = useForm({
+        defaultValues: {
+            label: 'Default',
+            description: 'Default',
+            complete: false
+        }
+    });
 
     return (
         <PageLayout title='Detalhes'>
-            Detail {id}
+            <form onSubmit={handleSubmit((data) => console.log(data))}>
+                <input {...register('label')} />
+                <input {...register('description')} />
+                <input type='checkbox' {...register('complete')} />
+
+                <button type="submit">
+                    Submit
+                </button>
+            </form>
         </PageLayout>
     );
 }
